c174be8527ad2988c0da27ccce2f80cdc6c07381
Dhairya Rana <cloudofdhairya@gmail.com>
2025-02-16 13:31:50 -0700

    script method

--- script.js
+++ script.js
@@ -14,8 +14,3 @@ function updateTime() {
 updateTime();
 setInterval(updateTime, 1000);
 
-$(document).ready(function() {
-    setTimeout(function() {
-        $(".bottom").html('<img src="../image/WhatsApp Image 2025-02-16 at 12.39.40_07f1cdae.jpg" width="300" height="300" alt="">');
-    }, 5000); // 5000ms = 5 seconds
-});
